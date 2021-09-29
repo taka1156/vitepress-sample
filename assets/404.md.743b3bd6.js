9619c013c708b62c59bc06a8c6f9d6a15f72c96a
taka1156 <taka1156@users.noreply.github.com>
2021-09-29 16:35:41 +0000

    deploy: 8d5dd4c9259847650f2297369483f2b8e8fc0ef1

---
+++ assets/404.md.743b3bd6.js
@@ -0,0 +1,1 @@
+import{_ as t,r as a,c as o,d as n,o as s}from"./app.aff72adb.js";const u='{"title":"","description":"","frontmatter":{"sidebar":false,"editLink":false},"relativePath":"404.md","lastUpdated":1632933329713}',r={};function c(d,p,_,i,l,f){const e=a("NotFound");return s(),o("div",null,[n(e)])}var v=t(r,[["render",c]]);export{u as __pageData,v as default};
